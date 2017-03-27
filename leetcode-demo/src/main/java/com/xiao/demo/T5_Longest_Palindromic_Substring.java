1ca95d85982446854244dc72d39abb171555d8aa
abuzhi <abuzhi@163.com>
2017-03-27 23:08:06 +0800

    letcode

--- leetcode-demo/src/main/java/com/xiao/demo/T5_Longest_Palindromic_Substring.java
+++ leetcode-demo/src/main/java/com/xiao/demo/T5_Longest_Palindromic_Substring.java
@@ -2,6 +2,34 @@ package com.xiao.demo;
 
 /**
  * Created by xiaoliang on 2017/3/26.
+ *
+ * Given a string s, find the longest palindromic substring in s. You may assume that the maximum length of s is 1000.
+
+ Example:
+
+ Input: "babad"
+
+ Output: "bab"
+
+ Note: "aba" is also a valid answer.
+ Example:
+
+ Input: "cbbd"
+
+ Output: "bb"
+
  */
 public class T5_Longest_Palindromic_Substring {
+
+    public String longestPalindrome(String s) {
+        char[] arrs = s.toCharArray();
+        int maxLen = 0;
+        String result = "";
+
+        for(int i =0,len = arrs.length;i<len;i++){
+            
+        }
+
+        return "";
+    }
 }
---
+++ leetcode-demo/src/main/java/com/xiao/demo/T9_Palindrome_Number.java
@@ -0,0 +1,21 @@
+package com.xiao.demo;
+
+/**
+ * Created by xiaoliang on 2017/3/27.
+ *
+ * Determine whether an integer is a palindrome. Do this without extra space.
+
+ click to show spoilers.
+
+ Some hints:
+ Could negative integers be palindromes? (ie, -1)
+
+ If you are thinking of converting the integer to string, note the restriction of using extra space.
+
+ You could also try reversing an integer. However, if you have solved the problem "Reverse Integer", you know that the reversed integer might overflow. How would you handle such case?
+
+ There is a more generic way of solving this problem.
+
+ */
+public class T9_Palindrome_Number {
+}
