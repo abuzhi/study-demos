ef0aab2aa633a720bba4e8abbfb083dfd2c73772
abuzhi <abuzhi@163.com>
2017-03-30 22:47:14 +0800

    letcode

--- leetcode-demo/src/main/java/com/xiao/demo/T5_Longest_Palindromic_Substring.java
+++ leetcode-demo/src/main/java/com/xiao/demo/T5_Longest_Palindromic_Substring.java
@@ -2,32 +2,37 @@ package com.xiao.demo;
 
 /**
  * Created by xiaoliang on 2017/3/26.
- *
+ * <p>
  * Given a string s, find the longest palindromic substring in s. You may assume that the maximum length of s is 1000.
-
- Example:
-
- Input: "babad"
-
- Output: "bab"
-
- Note: "aba" is also a valid answer.
- Example:
-
- Input: "cbbd"
-
- Output: "bb"
-
+ * <p>
+ * Example:
+ * <p>
+ * Input: "babad"
+ * <p>
+ * Output: "bab"
+ * <p>
+ * Note: "aba" is also a valid answer.
+ * Example:
+ * <p>
+ * Input: "cbbd"
+ * <p>
+ * Output: "bb"
  */
 public class T5_Longest_Palindromic_Substring {
 
     public String longestPalindrome(String s) {
         char[] arrs = s.toCharArray();
-        int maxLen = 0;
-        String result = "";
+        int maxLen = 0, len = arrs.length;
 
-        for(int i =0,len = arrs.length;i<len;i++){
-            
+        String result = "";
+        if (arrs.length < 2) {
+            return s;
+        }
+
+        boolean is = len % 2 == 0 ? true : false;
+
+        for (int i = 0; i < (len/2 + 1); i++) {
+
         }
 
         return "";
