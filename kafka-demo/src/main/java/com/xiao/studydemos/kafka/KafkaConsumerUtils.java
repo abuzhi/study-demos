e9d66c8cf4e0f5f589a7a412a6888d2a8b43d1a9
tianxiaoliang <tianxiaoliang@pwrd.com>
2017-12-14 16:31:18 +0800

    pom

---
+++ kafka-demo/src/main/java/com/xiao/studydemos/kafka/KafkaConsumerUtils.java
@@ -0,0 +1,75 @@
+package com.xiao.studydemos.kafka;
+
+import org.apache.kafka.clients.consumer.Consumer;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+
+/**
+ * Created by xiaoliang
+ * 2016.12.06 15:54
+ *
+ * @Version 1.0
+ */
+public class KafkaConsumerUtils {
+
+    private static final Logger logger = LoggerFactory.getLogger(KafkaConsumerUtils.class);
+
+    private static final String CONFIG_FILE = "config/kafka.properties";
+    private static Properties properties = null;
+    private static Consumer consumer = null;
+
+    private KafkaConsumerUtils(){
+
+    }
+
+    public static synchronized void init(String filepath){
+        properties = new Properties();
+        FileInputStream fis = null;
+        try{
+            String path = CONFIG_FILE;
+            if(filepath!=null && !"".equalsIgnoreCase(filepath)){
+                path = filepath;
+            }
+            File pfile = new File(path);
+            fis = new FileInputStream(pfile);
+            properties.load(fis);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 默认文件
+     */
+    public static synchronized void init(){
+        init("");
+    }
+
+    public static Consumer consumer(){
+        if(consumer==null){
+            consumer = new KafkaConsumer(consumerProps());
+        }
+        return consumer;
+    }
+
+    private static Map<String, Object> consumerProps() {
+        Map<String, Object> props = new HashMap<>();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, properties.getProperty("consumer.bootstrap.servers"));
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, properties.getProperty("consumer.group.id"));
+        props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, properties.getProperty("consumer.enable.auto.commit","true"));
+        props.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, properties.getProperty("consumer.auto.commit.interval.ms","100"));
+        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, properties.getProperty("consumer.auto.offset.reset","latest"));
+        props.put(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG, properties.getProperty("consumer.session.timeout.ms","10000"));
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, properties.getProperty("consumer.key.deserializer"));
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, properties.getProperty("consumer.value.deserializer"));
+        return props;
+    }
+}
---
+++ kafka-demo/src/main/java/com/xiao/studydemos/kafka/KafkaProducerUtils.java
@@ -0,0 +1,72 @@
+package com.xiao.studydemos.kafka;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+
+/**
+ * Created by xiaoliang
+ * 2016.12.06 15:54
+ *
+ * @Version 1.0
+ */
+public class KafkaProducerUtils {
+
+    private static final String CONFIG_FILE = "config/kafka.properties";
+    private static Properties properties = null;
+    private static Producer producer = null;
+
+    private KafkaProducerUtils(){
+
+    }
+
+    public static synchronized void init(String filepath){
+        properties = new Properties();
+        FileInputStream fis = null;
+        try{
+            File tmp = new File("");
+            String path =tmp.getCanonicalPath()+"/"+ CONFIG_FILE;
+            if(filepath!=null && !"".equalsIgnoreCase(filepath)){
+                path = tmp.getCanonicalPath()+"/"+ filepath;
+            }
+            System.out.println("init kafka producer : file ="+ path);
+            File pfile = new File(path);
+            fis = new FileInputStream(pfile);
+            properties.load(fis);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 默认文件
+     */
+    public static synchronized void init(){
+        init("");
+    }
+
+    public static Producer producer(){
+        if(producer==null){
+            producer = new KafkaProducer<>(producerProps());
+        }
+        return producer;
+    }
+
+    private static Map<String, Object> producerProps() {
+        Map<String, Object> props = new HashMap<>();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, properties.getProperty("producer.bootstrap.servers"));
+        props.put(ProducerConfig.RETRIES_CONFIG, 3);
+        props.put(ProducerConfig.BATCH_SIZE_CONFIG, 16384);
+        props.put(ProducerConfig.LINGER_MS_CONFIG, 1);
+        props.put(ProducerConfig.BUFFER_MEMORY_CONFIG, 33554432);
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, properties.getProperty("producer.key.serializer"));
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, properties.getProperty("producer.value.serializer"));
+        return props;
+    }
+}
