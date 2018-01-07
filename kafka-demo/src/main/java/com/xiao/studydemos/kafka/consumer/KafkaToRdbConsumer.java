1da38cda0a2aaec5c51af5f8e8bacab71c372870
tianxiaoliang <tianxiaoliang@pwrd.com>
2018-01-07 11:48:51 +0800

    y

--- kafka-demo/src/main/java/com/xiao/studydemos/kafka/consumer/KafkaToRdbConsumer.java
+++ kafka-demo/src/main/java/com/xiao/studydemos/kafka/consumer/KafkaToRdbConsumer.java
@@ -1,8 +1,42 @@
 package com.xiao.studydemos.kafka.consumer;
 
+import com.alibaba.fastjson.JSONObject;
+import com.xiao.demo.commons.utils.JdbcUtils;
+
+import java.sql.Connection;
+import java.util.Properties;
+
 /**
  * Created by xiaoliang on 2018/1/4.
  */
-public class KafkaToRdbConsumer {
+public class KafkaToRdbConsumer implements Runnable{
 
+    private Connection connection = null;
+    private  JdbcUtils jdbc = null;
+
+    public void init(Properties properties) throws Exception{
+        JdbcUtils jdbc = new JdbcUtils(properties);
+        connection = jdbc.getConnection();
+        connection.setAutoCommit(false);
+    }
+
+    public void close() throws Exception{
+        if(jdbc!=null){
+            jdbc.closeConnection(connection);
+        }
+    }
+
+    @Override
+    public void run() {
+
+    }
+
+    public boolean consumer(String msg) throws Exception {
+        JSONObject object = JSONObject.parseObject(msg);
+        String tableName = "t_test_"+ object.getString("id");
+
+        String sql = "insert " + tableName + " ";
+
+        return true;
+    }
 }
--- kafka-demo/src/test/java/com/xiao/studydemos/kafka/KafkaConsumerUtilsTest.java
+++ kafka-demo/src/test/java/com/xiao/studydemos/kafka/KafkaConsumerUtilsTest.java
@@ -5,15 +5,14 @@ import com.xiao.studydemos.kafka.utils.KafkaConsumerUtils;
 import org.apache.kafka.clients.consumer.Consumer;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.OffsetAndMetadata;
 import org.apache.kafka.common.PartitionInfo;
+import org.apache.kafka.common.TopicPartition;
 import org.junit.Before;
 import org.junit.Test;
 
 import java.io.File;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
-import java.util.Properties;
+import java.util.*;
 
 /**
  * Created by xiaoliang
@@ -41,13 +40,25 @@ public class KafkaConsumerUtilsTest extends BaseTest{
         int count = 100;
         while (true) {
             ConsumerRecords<String, String> records = consumer.poll(100);
-            for (ConsumerRecord<String, String> record : records){
-                count -- ;
-                System.out.printf("part=%d ,offset = %d, key = %s, value = %s%n ",record.partition(), record.offset(), record.key(), record.value());
-                if(count<0){
-                    break;
+            records.partitions();
+            for (TopicPartition partition : records.partitions()) {
+
+                List<ConsumerRecord<String, String>> partitionRecords = records.records(partition);
+                for (ConsumerRecord<String, String> record : partitionRecords) {
+                    System.out.printf("part=%d ,offset = %d, key = %s, value = %s%n ",record.partition(), record.offset(), record.key(), record.value());
                 }
+
+                long lastOffset = partitionRecords.get(partitionRecords.size() - 1).offset();
+                consumer.commitSync(Collections.singletonMap(partition, new OffsetAndMetadata(lastOffset + 1)));
             }
+//
+//            for (ConsumerRecord<String, String> record : records){
+//                count -- ;
+//                System.out.printf("part=%d ,offset = %d, key = %s, value = %s%n ",record.partition(), record.offset(), record.key(), record.value());
+//                if(count<0){
+//                    break;
+//                }
+//            }
         }
     }
 
