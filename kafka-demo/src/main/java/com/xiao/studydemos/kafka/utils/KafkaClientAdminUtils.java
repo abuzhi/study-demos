52dcf86a35b38c1d1b53df829a4368496f75cdc7
wangbadada <3041846076@qq.com>
2018-01-07 21:36:54 +0800

    kafka admin test

--- kafka-demo/src/main/java/com/xiao/studydemos/kafka/utils/KafkaClientAdminUtils.java
+++ kafka-demo/src/main/java/com/xiao/studydemos/kafka/utils/KafkaClientAdminUtils.java
@@ -65,4 +65,6 @@ public class KafkaClientAdminUtils {
         }
         return tmp;
     }
+
+
 }
--- kafka-demo/src/test/java/com/xiao/studydemos/kafka/utils/KafkaClientAdminUtilsTest.java
+++ kafka-demo/src/test/java/com/xiao/studydemos/kafka/utils/KafkaClientAdminUtilsTest.java
@@ -1,10 +1,15 @@
 package com.xiao.studydemos.kafka.utils;
 
 import com.xiao.studydemos.BaseTest;
+import org.apache.kafka.clients.admin.DescribeLogDirsResult;
+import org.apache.kafka.clients.admin.DescribeReplicaLogDirsResult;
 import org.apache.kafka.clients.admin.TopicDescription;
 import org.apache.kafka.clients.admin.TopicListing;
 import org.apache.kafka.common.Node;
+import org.apache.kafka.common.TopicPartition;
 import org.apache.kafka.common.TopicPartitionInfo;
+import org.apache.kafka.common.TopicPartitionReplica;
+import org.apache.kafka.common.requests.DescribeLogDirsResponse;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -67,5 +72,53 @@ public class KafkaClientAdminUtilsTest extends BaseTest{
     }
 
 
+    @Test
+    public void describeLogDirs() throws Exception {
+        Collection<Node> nodes = clientAdminUtils.describeCluster();
 
+        Set<Integer> hosts = new HashSet<>();
+
+        for(Node node : nodes){
+            hosts.add(node.id());
+            System.out.println(node.host() + " || "+ node.port() + " || "+ node.id() + " || "+ node.rack());
+        }
+
+        DescribeLogDirsResult result = clientAdminUtils.getAdminClient().describeLogDirs(hosts);
+        Map<Integer, Map<String, DescribeLogDirsResponse.LogDirInfo>> map = result.all().get();
+
+        for(Map.Entry<Integer, Map<String, DescribeLogDirsResponse.LogDirInfo>> entry : map.entrySet()){
+            System.out.println(entry.getKey());
+            Map<String, DescribeLogDirsResponse.LogDirInfo> logMap = entry.getValue();
+            for(Map.Entry<String, DescribeLogDirsResponse.LogDirInfo> log : logMap.entrySet()){
+                System.out.println(log.getKey());
+                DescribeLogDirsResponse.LogDirInfo info = log.getValue();
+                Map<TopicPartition, DescribeLogDirsResponse.ReplicaInfo> repMap = info.replicaInfos;
+                System.out.println(repMap.toString());
+            }
+        }
+    }
+
+    @Test
+    public void describeRepliaDirs() throws Exception {
+        TopicPartitionReplica t0 = new TopicPartitionReplica("test_topic",0,190);
+        TopicPartitionReplica t1 = new TopicPartitionReplica("test_topic",1,191);
+        TopicPartitionReplica t2 = new TopicPartitionReplica("test_topic",2,192);
+        Collection<TopicPartitionReplica> parts = new HashSet<>();
+        parts.add(t0);
+        parts.add(t1);
+        parts.add(t2);
+
+        DescribeReplicaLogDirsResult result = clientAdminUtils.getAdminClient().describeReplicaLogDirs(parts);
+        Map<TopicPartitionReplica, DescribeReplicaLogDirsResult.ReplicaLogDirInfo> map = result.all().get();
+        for(Map.Entry<TopicPartitionReplica, DescribeReplicaLogDirsResult.ReplicaLogDirInfo> entry :map.entrySet()){
+            TopicPartitionReplica replica = entry.getKey();
+            System.out.println(replica.brokerId() + " || "+ replica.partition() + " || "+ replica.topic());
+
+            DescribeReplicaLogDirsResult.ReplicaLogDirInfo info = entry.getValue();
+
+            System.out.println(info.getCurrentReplicaLogDir() + " || "+ info.getCurrentReplicaOffsetLag() + " || "+
+                info.getFutureReplicaLogDir() + " || "+ info.getFutureReplicaOffsetLag());
+        }
+
+    }
 }
