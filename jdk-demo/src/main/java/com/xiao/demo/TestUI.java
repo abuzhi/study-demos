fada37445bec553fa7e2de65325b96426b6fc601
tianxiaoliang <tianxiaoliang@pwrd.com>
2018-01-08 18:06:16 +0800

    kafka test

---
+++ jdk-demo/src/main/java/com/xiao/demo/TestUI.java
@@ -0,0 +1,13 @@
+package com.xiao.demo;
+
+import javax.swing.*;
+
+/**
+ * Created by xiaoliang
+ * 2018.01.04 09:44
+ *
+ * @Version 1.0
+ */
+public class TestUI {
+    private JPanel panel1;
+}
---
+++ kafka-demo/src/main/java/com/xiao/studydemos/kafka/steams/KafkaStreamConsumer.java
@@ -0,0 +1,12 @@
+package com.xiao.studydemos.kafka.steams;
+
+/**
+ * Created by xiaoliang
+ * 2018.01.05 15:10
+ *
+ * @Version 1.0
+ */
+public class KafkaStreamConsumer {
+
+
+}
