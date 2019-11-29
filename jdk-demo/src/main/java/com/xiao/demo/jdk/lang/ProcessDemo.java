748f2e49f7ef5897fd13cdc865e5a6b065bff5bc
abuzhi <abuzhi@163.com>
2019-11-29 16:47:37 +0800

    process

--- jdk-demo/src/main/java/com/xiao/demo/jdk/lang/ProcessDemo.java
+++ jdk-demo/src/main/java/com/xiao/demo/jdk/lang/ProcessDemo.java
@@ -7,4 +7,18 @@ package com.xiao.demo.jdk.lang;
  */
 public class ProcessDemo {
 
+    public static void main(String[] args) throws Exception {
+        System.out.println("This is a program test about Process, ProcessBuilder, Runtime.exec etc.");
+        System.out.println("Now Print the args:");
+        for(int i=0;i<args.length;i++){
+            System.out.println("	[args-"+i+"]:"+args[i]);
+        }
+        String sleepTime = args[0];
+        int k = Integer.valueOf(sleepTime);
+        while (k>0){
+            System.out.println(k);
+            k--;
+            Thread.sleep(1000);
+        }
+    }
 }
